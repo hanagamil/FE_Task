ca6a96476cfca799b281b738ffc9b74c63b6909e
Abd El-Moety <hanaa.gam.abd-el-moety@dxc.com>
2022-06-13 16:05:29 +0200

    Req #4 - 404 Page in using ui-router

--- app/app.routes.js
+++ app/app.routes.js
@@ -4,7 +4,7 @@ angular.module('appModule')
       enabled: true,
     });
   })
-  .config(($stateProvider) => {
+  .config(($stateProvider, $urlRouterProvider) => {
     $stateProvider
       .state({
         name: 'app',
@@ -17,5 +17,11 @@ angular.module('appModule')
         name: 'team-performance',
         url: '/team-performance',
         template: '<v-performance-page></v-performance-page>',
+      })
+      .state({
+        name: 'not-found',
+        url: '/404',
+        template: '<v-not-found-page></v-not-found-page>',
       });
+    $urlRouterProvider.otherwise('/404');
   });
